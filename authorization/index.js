a82f020810bdbba935d3211b2fc3959ab60380b1
Jóhan Davidsen <johan.davidsen@fjakkarin.com>
2016-10-20 11:16:17 +0200

    Added a authorization module to the server

---
+++ authorization/index.js
@@ -0,0 +1,46 @@
+/** Load dependencies */
+let koa = require('koa')
+let router = require('koa-router')({ prefix: '/authorization' })
+
+/** Setup the application settings */
+let app = koa()
+app.name = 'api-authorization'
+
+// Create the authorization route GET
+router.get('/', function *() {
+  this.body = 'Home page'
+})
+
+// Create the authorization route PUT
+router.put('/', function *() {
+  this.throw('Not Implemented', 501)
+})
+
+// Create the authorization route PATCH
+router.patch('/', function *() {
+  this.throw('Not Implemented', 501)
+})
+
+// Create the authorization route DELETE
+router.delete('/', function *() {
+  this.throw('Not Implemented', 501)
+})
+
+// Create the authorization route POST
+router.post('/', function *() {
+  this.body = 'api'
+})
+
+app.use(function *(next) {
+  try {
+    yield next
+  } catch (err) {
+    this.status = err.status || 500
+    this.body = err.message
+    this.app.emit('error', err, this)
+  }
+})
+
+app.use(router.routes())
+
+app.listen(3003)
